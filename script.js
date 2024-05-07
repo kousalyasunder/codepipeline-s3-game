fefcb909b97c1e441ba6e35fbf588f6a6c328023
kousalyasunder <169170895+kousalyasunder@users.noreply.github.com>
2024-05-07 20:34:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const cardArray = [
         { name: 'card1', img: 'images/Aurora.png' },
         { name: 'card1', img: 'images/Aurora.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card5', img: 'images/success.png' },
-        { name: 'card5', img: 'images/success.png' },
+        { name: 'card2', img: '' },
+        { name: 'card2', img: '' },
+        { name: 'card3', img: '' },
+        { name: 'card3', img: '' },
+        { name: 'card4', img: '' },
+        { name: 'card4', img: '' },
+        { name: 'card5', img: '' },
+        { name: 'card5', img: '' },
         // ...add more pairs as needed
     ];
 
